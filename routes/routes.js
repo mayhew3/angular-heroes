b70b68c1640c80dc0e66cefdeb418931e552752b
Mayhew Seavey <commander.boffo@gmail.com>
2018-10-31 16:29:32 -0700

    Attach to node server

---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+module.exports = function(app) {
+  let heroes = require('../controllers/hero_controller');
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      console.log(err.message);
+      console.log(err.stack);
+      console.log("Status: " + err.status);
+      res.status(err.status || 500).json({
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
+    console.log(err.message);
+    console.log(err.stack);
+    console.log("Status: " + err.status);
+    res.status(err.status || 500).json({
+      message: err.message,
+      error: err
+    });
+  });
+
+};
