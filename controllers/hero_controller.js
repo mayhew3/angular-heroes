04afe1ec81e3ef927154a3ff38b01982d21d1977
Mayhew Seavey <commander.boffo@gmail.com>
2018-10-31 17:08:05 -0700

    Bit of cleanup

--- controllers/hero_controller.js
+++ controllers/hero_controller.js
@@ -1,10 +1,27 @@
-const db = require('./database_util');
+const Sequelize = require('sequelize');
+let config = process.env.DATABASE_URL;
+const sequelize = new Sequelize(config, {
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false
+});
+
+const Hero = sequelize.define("hero", {
+  id: { type: Sequelize.INTEGER, primaryKey: true },
+  name: Sequelize.TEXT
+}, {
+  freezeTableName: true,
+  createdAt: false,
+  updatedAt: false
+});
 
 exports.getHeroes = function(request, response) {
-  console.log("Getting list of heroes...");
-
-  let sql = "SELECT * " +
-    "FROM hero ";
-
-  return db.findAll(response);
+  Hero.findAll().then(heroes => {
+    response.json(heroes);
+  })
 };
