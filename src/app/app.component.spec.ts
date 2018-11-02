79e1517a6273f7ca11db07784f6abeadecdd5e2d
Mayhew Seavey <commander.boffo@gmail.com>
2018-11-02 15:25:58 -0700

    App component test works now with mock components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {Component} from '@angular/core';
+import {RouterLinkDirectiveStub} from '../testing/router-link-directive-stub';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterLinkDirectiveStub,
+        MessagesStubComponent,
+        RouterOutletStubComponent
       ],
     }).compileComponents();
   }));
@@ -19,13 +24,20 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heroes');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-tour-of-heroes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   });
 });
+
+@Component({selector: 'app-messages', template: ''})
+class MessagesStubComponent {}
+
+// tslint:disable-next-line
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent { }
